c1e667dd5890b6371033d8beea1187d39682927e
Alberto Riva <alberto.riva87@gmail.com>
2015-02-20 14:58:18 +0100

    add email

--- main.rb
+++ main.rb
@@ -5,21 +5,27 @@ require 'pry'
 
 class Form
 
-   attr_reader :array_form
+   attr_reader :array_message , :array_mail
    def initialize
-     @array_form = []
+     @array_message = []
+     @array_mail = []
    end
 end
 
 f = Form.new
 
 get "/" do
-  @messages = f.array_form
+  @messages = f.array_message
+  @emails = f.array_mail
   erb :index
 end
 
 post "/" do
-  @messages = f.array_form
-  f.array_form << params[:message]
+  @messages = f.array_message
+  @emails = f.array_mail
+  f.array_message << params[:message]
+  f.array_mail << params[:email]
+
+
   erb :index
 end
