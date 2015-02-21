c564a19b2b519703cbadf0b0c94fd3a002f82b7b
Alberto Riva <alberto.riva87@gmail.com>
2015-02-21 14:53:54 +0100

    add error

--- main.rb
+++ main.rb
@@ -4,42 +4,50 @@ require 'pry'
 
 
 class Form
-
-   attr_reader :messages
-   def initialize
-     @messages = []
-   end
+  attr_reader :messages
+  def initialize
+    @messages = []
+  end
 end
 
 def is_a_valid_email?(email)
-    email_regex = %r{
-      ^ # Start of string
-      [0-9a-z] # First character
-      [0-9a-z.+]+ # Middle characters
-      [0-9a-z] # Last character
-      @ # Separating @ character
-      [0-9a-z] # Domain name begin
-      [0-9a-z.-]+ # Domain name middle
-      [0-9a-z] # Domain name end
-      $ # End of string
+  email_regex = %r{
+    ^ # Start of string
+    [0-9a-z] # First character
+    [0-9a-z.+]+ # Middle characters
+    [0-9a-z] # Last character
+    @ # Separating @ character
+    [0-9a-z] # Domain name begin
+    [0-9a-z.-]+ # Domain name middle
+    [0-9a-z] # Domain name end
+    $ # End of string
     }xi # Case insensitive
-
-    (email =~ email_regex)
+    return (email =~ email_regex) == 0
 end
 
-f = Form.new
+  f = Form.new
 
-get "/" do
-  @messages = f.messages
-  erb :index
-end
-
-post "/" do
-  @messages = f.messages
-  if is_a_valid_email?(params[:email]) == 0
-    @messages << {email:params[:email] , message:params[:message]}
-  else
-    @error_message = "errore email non valida, riscrivere"
+  get "/" do
+    @messages = f.messages
+    erb :index
   end
-erb :index
-end
+
+  post "/" do
+    @messages = f.messages
+
+
+
+    if !(is_a_valid_email?(params[:email])) && params[:message] == ""
+       @error_message = "messaggio vuoto"
+       @error_email = "email non corretta"
+    elsif  params[:message] == ""
+      @error_message = "messaggio vuoto"
+    elsif  !(is_a_valid_email?(params[:email]))
+      @error_email = "email non corretta"
+    else
+      @messages << {email:params[:email] , message:params[:message]}
+    end
+
+
+    erb :index
+  end
