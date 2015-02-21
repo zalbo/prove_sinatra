7347fcd520fa582a6c2c12cf8b5a546721375ea0
Matteo Parmi <m.parmi@gmail.com>
2015-02-21 17:53:59 +0100

    small form validation refactoring

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-
 require 'sinatra'
 require 'pry'
 
@@ -12,42 +11,50 @@ end
 
 def is_a_valid_email?(email)
   email_regex = %r{
-    ^ # Start of string
-    [0-9a-z] # First character
-    [0-9a-z.+]+ # Middle characters
-    [0-9a-z] # Last character
-    @ # Separating @ character
-    [0-9a-z] # Domain name begin
-    [0-9a-z.-]+ # Domain name middle
-    [0-9a-z] # Domain name end
-    $ # End of string
-    }xi # Case insensitive
-    return (email =~ email_regex) == 0
+  ^ # Start of string
+  [0-9a-z] # First character
+  [0-9a-z.+]+ # Middle characters
+  [0-9a-z] # Last character
+  @ # Separating @ character
+  [0-9a-z] # Domain name begin
+  [0-9a-z.-]+ # Domain name middle
+  [0-9a-z] # Domain name end
+  $ # End of string
+  }xi # Case insensitive
+  return (email =~ email_regex) == 0
 end
 
-  f = Form.new
-
-  get "/" do
-    @messages = f.messages
-    erb :index
+def validate_form(params)
+  errors = {}
+  if params[:email] == ""
+    errors[:email] = "email is empty"
   end
 
-  post "/" do
-    @messages = f.messages
-
-
-
-    if !(is_a_valid_email?(params[:email])) && params[:message] == ""
-       @error_message = "messaggio vuoto"
-       @error_email = "email non corretta"
-    elsif  params[:message] == ""
-      @error_message = "messaggio vuoto"
-    elsif  !(is_a_valid_email?(params[:email]))
-      @error_email = "email non corretta"
-    else
-      @messages << {email:params[:email] , message:params[:message]}
-    end
-
-
-    erb :index
+  if errors.empty? && !is_a_valid_email?(params[:email])
+    errors[:email] = "email is not valid"
   end
+
+  if params[:message] == ""
+    errors[:message] = "message is empty"
+  end
+  errors
+end
+
+f = Form.new
+
+get "/" do
+  @messages = f.messages
+  @errors = {}
+  erb :index
+end
+
+post "/" do
+  @messages = f.messages
+  @errors = validate_form(params)
+
+  if @errors.empty?
+    @messages << {email:params[:email] , message:params[:message]}
+  end
+
+  erb :index
+end
